c9302e962d39fafd5076fcf21cd4fda377fbdaa0
TinsoftTech <66064134+TinsoftTech@users.noreply.github.com>
2020-07-16 14:29:23 +0100

    Update followViewModel.js

--- viewmodel/followViewModel.js
+++ viewmodel/followViewModel.js
@@ -72,7 +72,7 @@ app.post("/api/follow/addnew/",urlencodedParser,function(req,res){
                     var response = {
                         "result":{
                         "responsecode":0,
-                         "status":"Error found"
+                         "status":"Error founddd"
                         }
                      }
                      res.json(response);
@@ -91,30 +91,6 @@ app.post("/api/follow/addnew/",urlencodedParser,function(req,res){
               //  res.render("todo",{todos:data});
               //  console.log('product saved');
                 });
-        } else {
-            Todo.findOne({$and:[{user:req.body.user.replace(/\-/g," ")},{storeid:req.body.storeid.replace(/\-/g," ")}]}).remove(function(err,data){
-
-                if (err) 
-                {
-                    var response = {
-                        "result":{
-                         "responsecode":0,   
-                         "status":"Error"
-                        }
-                     }
-                }
-                else{
-                    var response = {
-                        "result":{
-                         "responsecode":1,   
-                         "status":"Removed Succesfully"
-                        }
-                     }
-                }
-                res.json(response)
-                
-                
-                }) 
         }
       //  res.send({follow:data});
 //console.log({storename:req.params.name});
